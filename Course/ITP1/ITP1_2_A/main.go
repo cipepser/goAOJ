bd8d52e6d717beff0dc0b5dd1c32254cc541dfd3
cipepser <respepic@gmail.com>
2018-10-10 07:55:54 +0900

    add ITP1_2_A

---
+++ Course/ITP1/ITP1_2_A/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	n, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+
+	a := nextInt(sc)
+	b := nextInt(sc)
+	if a < b {
+		fmt.Println("a < b")
+	} else if a > b {
+		fmt.Println("a > b")
+	} else {
+		fmt.Println("a == b")
+	}
+}

cdf10daf9ba5a55fa148fa64c33cd95977006a8a
cipepser <respepic@gmail.com>
2018-10-17 07:55:43 +0900

    add ITP1_3_D

---
+++ Course/ITP1/ITP1_3_D/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	n, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	a := nextInt(sc)
+	b := nextInt(sc)
+	c := nextInt(sc)
+
+	cnt := 0
+	for i := a; i <= b; i++ {
+		if c%i == 0 {
+			cnt++
+		}
+	}
+	fmt.Println(cnt)
+}
